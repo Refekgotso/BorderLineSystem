686a3060030d4163ce77da4dae0078e5a97146af
Refekgotso <refekgotsoramaano@gmail.com>
2026-07-15 16:14:06 +0200

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/BorderLineSystem/BorderLine/BorderLineApplication.java
@@ -0,0 +1,13 @@
+package com.BorderLineSystem.BorderLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorderLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorderLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BorderLineSystem/BorderLine/BorderLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BorderLineSystem.BorderLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorderLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
